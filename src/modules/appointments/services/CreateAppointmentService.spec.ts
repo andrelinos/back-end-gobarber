c48d6e008d9da609f38b6f92fa4dc6f8c5762a57
Andrelino Silva <andrelinocom@gmail.com>
2020-07-21 21:35:40 -0300

    Continuando Tests e Refator (RED/GREEN/REFATOR)

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -32,7 +32,7 @@ describe('CreateAppointment', () => {
       provider_id: '114545644654',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '114545644655',
