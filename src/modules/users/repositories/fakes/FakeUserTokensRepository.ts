aa2a81091a6314212c6991e934c11431016b5632
Andrelino Silva <andrelinocom@gmail.com>
2020-07-27 21:59:13 -0300

    Add Test Hash, Token, Passed hours... Reset Pass

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -14,9 +14,19 @@ class FakeUserTokensRepository implements IUsersTokensRespository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.UserTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.UserTokens.find(
+      findToken => findToken.token === token,
+    );
 
     return userToken;
   }
